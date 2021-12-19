60a6fb979d9e1bed99a63498c5c123cfdfdb1b59
VICTOR-81 <v161181@gmail.com>
2021-12-19 12:39:57 +0700

    continue

---
+++ js/swiper.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  const swiper = new Swiper('.swiper', {
+    
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 16,
+    slidesPerView: 2,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'progressbar',
+      progressbarFillClass: 'swiper-pagination-progressbar-fill',
+    },
+
+    breakpoints: {
+      578: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      },
+    }
+  });
+
+});
